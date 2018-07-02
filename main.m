0eefc2b9d1cb959843f8f4824e3c5fcac0a35ec2
Pablo Vicente Munuera <pvicente1@us.es>
2018-07-02 18:01:08 +0200

    coupling neurons and nuclei

--- main.m
+++ main.m
@@ -4,5 +4,5 @@ tifFiles = dir('Data/*.tif');
 
 for numFile = 1:length(tifFiles)
     actualFile = tifFiles(numFile);
-    segmentationRedZoneOfDamage(strcat(actualFile.folder, '/', actualFile.name));
+    processingImg(strcat(actualFile.folder, '/', actualFile.name));
 end
--- src/processingImg.m
+++ src/processingImg.m
@@ -1,10 +1,14 @@
 function [finalRedZone] = processingImg(pathFile)
-    % Channel 1: Neurons
-    % Channel 2: Nuclei
-    % Channel 3: Damage
-    % Channel 4: Perfusion 
+%%PROCESSINGIMG
+% Channel 1: Nuclei (Blue)
+% Channel 2: Neurons (Green)
+% Channel 3: Damage (Red)
+% Channel 4: Perfusion (White)
 
-    %Getting all the 
+    pixelWidthInMicrons = 0.3031224;
+    minCellSizeInPixels = 3/pixelWidthInMicrons;
+
+    %Getting all the initial information
     rawImg=imfinfo(pathFile);
     maxValue = 4095;
     for numChannel = 1:length(rawImg)
@@ -23,5 +27,38 @@ function [finalRedZone] = processingImg(pathFile)
     redZoneFilled = imfill(redZone, 'holes');
     onlyRedZone = bwareafilt(redZoneFilled, 1);
     finalRedZone = imdilate(onlyRedZone, strel('disk', 2));
+    
+    %Locate neurons (Channel 1 and 2)
+%     figure; imshow(grayImages(:,:,1))
+%     figure; imshow(grayImages(:,:,2))
+    neuronsAndMore = imbinarize(grayImages(:, :, 2),'global');
+    %figure; imshow(neuronsAndMore)
+    neuronsAndMoreFilled = imfill(neuronsAndMore, 'holes');
+    
+    nuclei = imbinarize(grayImages(:, :, 1),'adaptive','Sensitivity',0.05);
+    %figure; imshow(nuclei);
+    nucleiFilled = imfill(nuclei, 'holes');
+    
+%     nucleiFilled = imfill(nuclei, 'holes');
+%     nuclei = nucleiFilled;
+%     distMatrix = bwdist(~nuclei);
+%     distMatrix = -distMatrix;
+%     distMatrix(~nuclei) = Inf;
+%     nucleiWatersheded = watershed(distMatrix);
+%     nucleiWatersheded(~nuclei) = 0;
+%     figure; imshow(double(nucleiWatersheded))
+    
+    %With segmented images works better
+    nucleiWithNeuron = imreconstruct(neuronsAndMoreFilled, nucleiFilled);
+    figure; imshow(nucleiWithNeuron);
+    %Remove smaller non-cell elements
+    bwareafilt(nucleiWithNeuron, [minCellSizeInPixels Inf]);
+    
+    %works worse
+    %figure; imshow(imreconstruct(grayImages(:, :, 2), grayImages(:, :,
+    %1)))
+    
+    %Calculate density
+    
     
 end
