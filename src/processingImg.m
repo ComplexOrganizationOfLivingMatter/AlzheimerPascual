44aa8bcf0ecfba79a2c26ce8169f46ef019ead64
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-07-16 16:55:57 +0200

    first approach to nuclei-cells segmentation -PEDRO

--- src/processingImg.m
+++ src/processingImg.m
@@ -4,49 +4,54 @@ function [finalRedZone] = processingImg(pathFile)
 % Channel 2: Neurons (Green)
 % Channel 3: Damage (Red)
 % Channel 4: Perfusion (White)
-    for nChan=1:4
-        imchan=imread(strrep(pathFile,'C=0',['C=' num2str(nChan)-1]));
-        grayImages(:,:,nChan)=imchan;
-    end
-    
+
     micronsOfSurroundingZone = 10;
-    minSizeCellInMicrons = 3;
+    minSizeCellInMicrons = 25;
+    minObjectSizeDeleteCellsInMicrons=5;
     
     pixelWidthInMicrons = 0.3031224;
     minCellSizeInPixels = ceil(minSizeCellInMicrons/pixelWidthInMicrons)^2;
     pixelsOfSurroundingZone = ceil(micronsOfSurroundingZone/pixelWidthInMicrons);
-
+    minObjectSizeInPixels2Delete= ceil(minObjectSizeDeleteCellsInMicrons/pixelWidthInMicrons)^2;
+    
     %% Getting all the initial information
-    rawImg=imfinfo(pathFile);
-    maxValue = 4095;
-    for numChannel = 1:length(rawImg)
-         actualImgChannel = imread(pathFile,numChannel);
-         rawImages(:, :, numChannel) = actualImgChannel;
-         
-         %Transform them to gray
-         actualImgChannelGray = ind2rgb(actualImgChannel, gray(maxValue));
-         actualImgChannelGray = actualImgChannelGray(:, :, 1);
-         
-         grayImages(:,:,numChannel) = actualImgChannelGray;
+%     rawImg=imfinfo(pathFile);
+%     maxValue = 4095;
+%     for numChannel = 1:length(rawImg)
+%          actualImgChannel = imread(pathFile,numChannel);
+%          rawImages(:, :, numChannel) = actualImgChannel;
+%          
+%          %Transform them to gray
+%          actualImgChannelGray = ind2rgb(actualImgChannel, gray(maxValue));
+%          actualImgChannelGray = actualImgChannelGray(:, :, 1);
+%          
+%          grayImages(:,:,numChannel) = actualImgChannelGray;
+%     end
+    
+    for nChan=1:4
+        imchan=imread(strrep(pathFile,'C=0',['C=' num2str(nChan)-1]));
+        grayImages(:,:,nChan)=imchan(:,:,1)+imchan(:,:,2)+imchan(:,:,3);
+        %to visualize image:
+        %figure;imshow(double(mat2gray(grayImages(:,:,nChan),[0,255])))
     end
     
     %% Damage zone (Channel 3)
     redZone = imbinarize(grayImages(:,:,3));
     redZoneFilled = imfill(redZone, 'holes');
     onlyRedZone = bwareafilt(redZoneFilled, 1);
-    finalRedZone = imdilate(onlyRedZone, strel('disk', 2));
+    finalRedZone = imdilate(onlyRedZone, strel('disk', 5));
     finalRedZone = imfill(finalRedZone, 'holes');
+    finalRedZone = imerode(finalRedZone, strel('disk', 5));
     
-    redZoneDilated = imdilate(finalRedZone, strel('disk', round(pixelsOfSurroundingZone/2)));
-    redZoneEroded = imerode(finalRedZone, strel('disk', round(pixelsOfSurroundingZone/2)));
-    %We catch a half of the red zone and half from outside it.
-    borderRedZone = ~redZoneEroded & redZoneDilated;
+    %We catch a half of the red zone and half from outside perimeter it.
+    perimeterRedZone=bwperim(finalRedZone);
+    borderRedZone = imdilate(perimeterRedZone, strel('disk', round(pixelsOfSurroundingZone/2)));
     
-    redZoneAreaInMicrons = sum(finalRedZone(:)) * pixelWidthInMicrons;
+    redZoneAreaInMicrons = sum(finalRedZone(:)) * pixelWidthInMicrons^2;
     %CARE: surrounding to red zone and outside red zone overlap between
     %each other
-    borderRedZoneAreaInMicrons = sum(borderRedZone(:)) * pixelWidthInMicrons;
-    outsideRedZoneAreaInMicrons = sum(finalRedZone(:) == 0) * pixelWidthInMicrons;
+    borderRedZoneAreaInMicrons = sum(borderRedZone(:)) * pixelWidthInMicrons^2;
+    outsideRedZoneAreaInMicrons = sum(finalRedZone(:) == 0) * pixelWidthInMicrons^2;
     
 %     figure; imshow(finalRedZone)
 %     figure; imshow(surroundingToRedZone)
@@ -56,12 +61,19 @@ function [finalRedZone] = processingImg(pathFile)
 %     figure; imshow(grayImages(:,:,1))
 %     figure; imshow(grayImages(:,:,2))
     neuronsAndMore = imbinarize(grayImages(:, :, 2),'global');
-    %figure; imshow(neuronsAndMore)
-    neuronsAndMoreFilled = imfill(neuronsAndMore, 'holes');
-    
-    nuclei = imbinarize(grayImages(:, :, 1),'adaptive','Sensitivity',0.05);
+    neuronsAndMoreAreaOpen = bwareaopen(neuronsAndMore,minObjectSizeInPixels2Delete);
+    neuronsAndMoreFilled = imfill(neuronsAndMoreAreaOpen, 'holes');
+    neuronsAndMoreErode=imerode(neuronsAndMoreFilled,strel('disk',2));
+    neuronsAndMoreDilate=imdilate(neuronsAndMoreErode,strel('disk',2));
+
+    %figure; imshow(neuronsAndMoreFilled)
+
+
+    nuclei = imbinarize(grayImages(:, :, 1));
+    nucleiOpen = bwareaopen(nuclei,minObjectSizeInPixels2Delete);
+
     %figure; imshow(nuclei);
-    nucleiFilled = imfill(nuclei, 'holes');
+    nucleiFilled = imfill(nucleiOpen, 'holes');
     
 %     nucleiFilled = imfill(nuclei, 'holes');
 %     nuclei = nucleiFilled;
