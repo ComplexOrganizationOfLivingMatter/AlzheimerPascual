7e4d70f3f9a300285d87de04e0acfe4a40150ea0
kapy95 <carcapagu@gmail.com>
2018-07-16 10:47:13 +0200

    first commit

--- main.m
+++ main.m
@@ -2,7 +2,9 @@
 addpath(genpath('src'));
 tifFiles = dir('Data/*.tif');
 
+
 for numFile = 1:length(tifFiles)
+    
     actualFile = tifFiles(numFile);
     processingImg(strcat(actualFile.folder, '/', actualFile.name));
 end
