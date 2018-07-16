436307e41da1b1c0d4f22c86abe15fdf2686b0d0
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-07-16 14:17:55 +0200

    cleaned main

--- main.m
+++ main.m
@@ -2,9 +2,8 @@ addpath(genpath('src'));
 tifFiles = dir('Data\NeuN*\**\*.tif');
 
 folderRows={tifFiles(:).folder}';
-namesRows={tifFiles(:).name}';
 [~, indicesFiles]=unique(folderRows);
-a=namesRows(indicesFiles);
+
 for numFile = indicesFiles'
     actualFile = tifFiles(numFile);
     processingImg(strcat(actualFile.folder, '/', actualFile.name));
