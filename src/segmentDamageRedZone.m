937fe14fff5927796201ee3c8ac4c82d7867bb48
Pablo Vicente Munuera <pvicente1@us.es>
2018-07-19 15:49:56 +0200

    plaque detection improved

--- src/segmentDamageRedZone.m
+++ src/segmentDamageRedZone.m
@@ -8,32 +8,28 @@ function [finalRedZone,redZoneAreaInMicrons,outsideRedZoneAreaInMicrons] = segme
     onlyRedZone=ismember(redZoneFilledLabelled,find(cat(1,areaRedZone.Area)>minRedAreaPixels));
     finalRedZone = imdilate(onlyRedZone, strel('disk', 5));
     finalRedZone = imfill(finalRedZone, 'holes');
-    finalRedZone = imerode(finalRedZone, strel('disk', 5));
-    
-    
+%     finalRedZone = imerode(finalRedZone, strel('disk', 5));
     
     %We catch a half of the red zone and half from outside perimeter it.
-    perimeterRedZone = bwperim(finalRedZone);
-    borderRedZone = imdilate(perimeterRedZone, strel('disk', round(pixelsOfSurroundingZone)));
-    finalRedZone = finalRedZone | borderRedZone;
+%     perimeterRedZone = bwperim(finalRedZone);
+%     borderRedZone = imdilate(perimeterRedZone, strel('disk', round(pixelsOfSurroundingZone)));
+%     finalRedZone = finalRedZone | borderRedZone;
     
+    
+
     redZoneAreaInMicrons = sum(finalRedZone(:)) * pixelWidthInMicrons^2;
     outsideRedZoneAreaInMicrons = sum(finalRedZone(:) == 0) * pixelWidthInMicrons^2;
     
     % Detecting plaque and removing it from red zone
     perfusionChannel = imbinarize(adapthisteq(grayImages(:,:,4)));
     plaqueDetection = perfusionChannel & imbinarize(adapthisteq(grayImages(:,:,1)), 'adaptive') & finalRedZone;
-    plaqueBigger = imbinarize(grayImages(:, :, 4), 'adaptive');
-    plaqueBiggerFilled = imfill(plaqueBigger, 'holes');
-    plaquesLabelled = bwlabel(plaqueBiggerFilled);
-    idsOfPlaque = unique(plaquesLabelled(plaqueDetection));
-    finalPlaques = ismember(plaquesLabelled, idsOfPlaque);
+
+    plaqueDetection = imdilate(plaqueDetection, strel('disk', 5));
+    plaqueDetection = imfill(plaqueDetection, 'holes');
+    plaqueDetection = imerode(plaqueDetection, strel('disk', 5));
+    plaqueDetection = bwareaopen(plaqueDetection, 300);
     
-    finalPlaques = imdilate(finalPlaques, strel('disk', 5));
-    finalPlaques = imfill(finalPlaques, 'holes');
-    finalPlaques = imerode(finalPlaques, strel('disk', 5));
-    
-    finalRedZone(finalPlaques) = 0;
+    finalRedZone(plaqueDetection) = 0;
     imwrite(finalRedZone, strcat(outputDir, '/redZoneSegmented.tif'));
     
 end
