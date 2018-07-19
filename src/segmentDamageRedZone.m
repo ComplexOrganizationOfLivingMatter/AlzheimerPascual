900a9af6ffee1482b057bc28c0e0b87bfe3b38cd
Pablo Vicente Munuera <pvicente1@us.es>
2018-07-19 16:41:15 +0200

    Using the nuclei channel to detect the plaque

--- src/segmentDamageRedZone.m
+++ src/segmentDamageRedZone.m
@@ -21,13 +21,22 @@ function [finalRedZone,redZoneAreaInMicrons,outsideRedZoneAreaInMicrons] = segme
     outsideRedZoneAreaInMicrons = sum(finalRedZone(:) == 0) * pixelWidthInMicrons^2;
     
     % Detecting plaque and removing it from red zone
-    perfusionChannel = imbinarize(adapthisteq(grayImages(:,:,4)));
-    plaqueDetection = perfusionChannel & imbinarize(adapthisteq(grayImages(:,:,1)), 'adaptive') & finalRedZone;
-
+    perfusionChannel = imbinarize(adapthisteq(grayImages(:,:,4)), 'adaptive', 'Sensitivity', 0.2);
+    plaqueDetection = perfusionChannel & finalRedZone; %imbinarize(adapthisteq(grayImages(:,:,1)), 'adaptive')
+    plaqueDetection = bwareaopen(plaqueDetection, 20);
     plaqueDetection = imdilate(plaqueDetection, strel('disk', 5));
     plaqueDetection = imfill(plaqueDetection, 'holes');
     plaqueDetection = imerode(plaqueDetection, strel('disk', 5));
-    plaqueDetection = bwareaopen(plaqueDetection, 300);
+    plaqueDetection = bwareaopen(plaqueDetection, 500);
+    
+    plaquesRegion = regionprops(plaqueDetection, 'all');
+    plaquesElongated = [vertcat(plaquesRegion.MinorAxisLength) ./ vertcat(plaquesRegion.MajorAxisLength) > 0.3];
+    plaqueDetection = ismember(bwlabel(plaqueDetection), find(plaquesElongated));
+    
+    % Using the nuclei channel to detect the plaque
+    plaqueDetection = plaqueDetection & imbinarize(adapthisteq(grayImages(:,:,1)), 'adaptive');
+    plaqueDetection = imfill(plaqueDetection, 'holes');
+    plaqueDetection = bwareaopen(plaqueDetection, 500);
     
     finalRedZone(plaqueDetection) = 0;
     imwrite(finalRedZone, strcat(outputDir, '/redZoneSegmented.tif'));
